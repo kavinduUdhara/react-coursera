cd0e9bca0aaf6c1143bcf066dffecf568bbcde46
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-04-01 18:43:18 +0530

    completed Principles of components: Props

--- app1/src/App.js
+++ app1/src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 
-function App() {
+function App(props) {
   return (
-  <Header/>
-    )
+    <h1>This is a {props.title}</h1>
+  )
 }
 
 export default App;
