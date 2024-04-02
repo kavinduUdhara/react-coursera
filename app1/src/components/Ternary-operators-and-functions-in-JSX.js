ea0904822957ce9b30fd3d8f56c0e275bf5f08c9
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-04-02 19:09:09 +0530

    added Ternary operators and functions in JSX file in components
    it talks about difference ways we can use if function like
    condition ? if condtion True statement : if condition False statement

--- app1/src/components/Ternary-operators-and-functions-in-JSX.js
+++ app1/src/components/Ternary-operators-and-functions-in-JSX.js
@@ -1,6 +1,8 @@
-let name = 'Bob';
-if (name == 'Bob') {
+var name2 = 'Bob';
+if (name2 == 'Bob') {
     console.log('Hello, Bob');
 } else {
     console.log('Hello, Friend');
 };
+var name1 = 'Bob';
+name1 == "Bob" ? console.log("this is the bob") : console.log("it's not a bob");
