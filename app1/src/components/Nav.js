0037aa0f68d42e132266c4c0053a36b7bce58856
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-04-02 18:33:17 +0530

    changed function declaration to a function expression on Nav

--- app1/src/components/Nav.js
+++ app1/src/components/Nav.js
@@ -1,7 +1,18 @@
-export default function Nav(props){
-    return(
+// export default function Nav(props){
+//     return(
+//         <ul>
+//             <li>{props.first}</li>
+//         </ul>
+//     )
+// }
+//writing above as a function expression
+
+const Nav = function(props) {
+    return (
         <ul>
             <li>{props.first}</li>
         </ul>
     )
 }
+
+export default Nav;
