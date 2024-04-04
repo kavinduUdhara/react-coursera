055719783df7d9f400b96e0ebd43a8c271358077
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-04-04 13:40:58 +0530

    completed

--- app1/src/App.js
+++ app1/src/App.js
@@ -6,20 +6,31 @@ import Pears from './components/Pears';
 import Bag from './components/Bag';
 import Nav from './components/Nav';
 
-const bool = false; 
+const bool = false;
+const str1 = "just";
 
 function Example(props) {
-    return (
-        <h2>The value of the toggleBoolean prop is: {props.toggleBoolean.toString()}</h2>
-    );
+  return (
+      <div>
+          <h2>
+              The value of the toggleBoolean prop is:{props.toggleBoolean.toString()}
+          </h2>
+          <p>The value of the math prop is: <em>{props.math}</em></p>
+          <p>The value of the str prop is: <em>{props.str}</em></p>
+      </div>
+  );
 };
 
 function App(props) {
   console.log(logo);
     return ( 
-        <div className="App"> 
-            <Example toggleBoolean={!bool} /> 
-        </div> 
+      <div className="App">
+        <Example
+            toggleBoolean={!bool}
+            math={(10 + 20) / 3}
+            str={str1 + ' another ' + 'string'}
+        />
+      </div>
     ); 
 }
 
