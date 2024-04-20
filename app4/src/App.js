45966b1f0310f7f9af1a3e7f976340ff707615c9
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-04-20 19:49:29 +0530

    completed Create a basic List component

--- app4/src/App.js
+++ app4/src/App.js
@@ -1,23 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = [
+  {
+  id: "1",
+  title: "Tiramisu",
+  description: "The best tiramisu in town",
+  image: "https://picsum.photos/200/300/?random",
+  price: "$5.00",
+  },
+  {
+  id: "2",
+  title: "Lemon Ice Cream",
+  description: "Mind blowing taste",
+  image: "https://picsum.photos/200/300/?random",
+  price: "$4.50",
+  },
+  {
+  id: "3",
+  title: "Chocolate mousse",
+  description: "Unexplored flavour",
+  image: "https://picsum.photos/200/300/7random",
+  price: "$6.00",
+  },
+];
+
+const desserts = [
+  {
+    name: "Chocolate Cake",
+    calories: 400,
+    createdAt: "2022-09-01",
+  },
+  {
+    name: "Ice Cream",
+    calories: 200,
+    createdAt: "2022-01-02",
+  },
+  {
+    name: "Tiramisu",
+    calories: 300,
+    createdAt: "2021-10-03",
+  },
+  {
+    name: "Cheesecake",
+    calories: 600,
+    createdAt: "2022-01-04",
+  },
+];
+
+const topDessert = data.map(dessert =>{
+  return{
+    content: `${dessert.title} - ${dessert.description}`,
+    price: dessert.price,
+  }
+})
+
 function App() {
+  const listItems = data.map(e => {
+    return(
+      <li>{`${e.id}. ${e.title} - ${e.price}`}</li>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Examine the console output</h1>
+      <ul>
+      {data.map(e =>{
+        return(
+            <li key={e.id}>{`${e.title} - ${e.description}`}</li>
+        )
+      })}
+      </ul>
+      {/*following is same as above but with extra function */}
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }
