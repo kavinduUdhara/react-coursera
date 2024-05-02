997772498d2faf52c0631403597ca3471ab184b3
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-05-02 12:35:37 +0530

    types of children done

--- app6/src/App.js
+++ app6/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Children, cloneElement } from 'react';
+
+function Raw({children, spacing, classNme}){
+  const childStyle = {
+    marginLeft: `${spacing}px`,
+  };
+
+  return(
+    <div className={classNme}>
+      {Children.map(children, (child, index) => {
+        return cloneElement(child, {
+          
+        });
+      })}
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Raw spacing={33} classNme="raw">
+        <p>Pizza</p>
+        <p>2</p>
+        <p>305</p>
+        <p>10:38</p>
+      </Raw>
     </div>
   );
 }
