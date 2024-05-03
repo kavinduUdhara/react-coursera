4b3547c13e9b4859c8a5625cb4a2f925e0dbb841
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-05-03 09:02:01 +0530

    added my world which is don't work

--- childClone/src/Radio/index.js
+++ childClone/src/Radio/index.js
@@ -1,20 +1,34 @@
-import * as React from "react";
+import React, { Children, cloneElement } from "react";
 import "./styles.css";
 
 export const RadioGroup = ({ onChange, selected, children }) => {
   // Use React.Children.map and React.cloneElement to clone the children
   // and pass the correct props to each RadioOption
-  const RadioOptions = null;
+  const num = Children.count(children)
+  const RadioOptions = Children.map(children, child => {
+    return cloneElement(child,
+      {checked: false}
+    )
+  });
 
-  return <div className="RadioGroup">{RadioOptions}</div>;
+  return (
+    <div>
+      <h1>{num}</h1>
+      <div className="RadioGroup">{RadioOptions}</div>
+    </div>
+  );
 };
+
+const handelChange = (e) => {
+  const newVarSelected = e.target.value;
+}
 
 export const RadioOption = ({ value, checked, onChange, children }) => {
   // Hook up the onChange handler to call the onChange prop passed to RadioGroup
   // Also, make sure to pass the correct checked prop to the input element
   return (
     <div className="RadioOption">
-      <input id={value} type="radio" name={value} />
+      <input id={value} type="radio" name="radio" onChange={handelChange}/>
       <label htmlFor={value}>{children}</label>
     </div>
   );
